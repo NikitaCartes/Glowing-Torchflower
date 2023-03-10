33a35a22e39a330d5d02516ac44c9f138e83c460
NikitaCartes <nikich98@yandex.ru>
2023-03-10 03:18:27 +0300

    Small optimization

--- src/main/java/xyz/nikitacartes/glowingtorchflower/GlowingTorchflower.java
+++
@@ -1,9 +0,0 @@
-package xyz.nikitacartes.glowingtorchflower;
-
-import net.fabricmc.api.ModInitializer;
-
-public class GlowingTorchflower implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
