31b3b942f3850c601c6ec307ef222ed64742b4e9
NikitaCartes <nikich98@yandex.ru>
2024-07-02 11:31:35 +0200

    Fix compatibility issues

--- src/main/java/xyz/nikitacartes/glowingtorchflower/mixin/BlocksMixin.java
+++ src/main/java/xyz/nikitacartes/glowingtorchflower/mixin/BlocksMixin.java
@@ -1,39 +1,39 @@
 package xyz.nikitacartes.glowingtorchflower.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.TorchflowerBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(Blocks.class)
 public class BlocksMixin {
 
-    @ModifyArg(method = "<clinit>",
+    @ModifyExpressionValue(method = "<clinit>",
             at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/block/FlowerBlock;<init>(Lnet/minecraft/registry/entry/RegistryEntry;FLnet/minecraft/block/AbstractBlock$Settings;)V",
+                    target = "Lnet/minecraft/block/AbstractBlock$Settings;create()Lnet/minecraft/block/AbstractBlock$Settings;",
                     ordinal = 0),
             slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=torchflower")))
     private static AbstractBlock.Settings modifyTorchflower(AbstractBlock.Settings properties) {
         return properties.luminance(blockState -> 12);
     }
 
-    @ModifyArg(method = "createFlowerPotBlock(Lnet/minecraft/block/Block;)Lnet/minecraft/block/Block;",
+    @ModifyExpressionValue(method = "createFlowerPotBlock(Lnet/minecraft/block/Block;)Lnet/minecraft/block/Block;",
             at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/block/FlowerPotBlock;<init>(Lnet/minecraft/block/Block;Lnet/minecraft/block/AbstractBlock$Settings;)V",
+                    target = "Lnet/minecraft/block/AbstractBlock$Settings;create()Lnet/minecraft/block/AbstractBlock$Settings;",
                     ordinal = 0))
-    private static AbstractBlock.Settings modifyPottedTorchflower(Block block, AbstractBlock.Settings properties) {
-        if (block.getTranslationKey().equals("block.minecraft.torchflower"))
+    private static AbstractBlock.Settings modifyPottedTorchflower(AbstractBlock.Settings properties, Block block) {
+        if (block == Blocks.TORCHFLOWER)
             return properties.luminance(blockState -> 14);
         return properties;
     }
 
-    @ModifyArg(method = "<clinit>",
+    @ModifyExpressionValue(method = "<clinit>",
             at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/block/TorchflowerBlock;<init>(Lnet/minecraft/block/AbstractBlock$Settings;)V",
+                    target = "Lnet/minecraft/block/AbstractBlock$Settings;create()Lnet/minecraft/block/AbstractBlock$Settings;",
                     ordinal = 0),
             slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=torchflower_crop")))
     private static AbstractBlock.Settings modifyTorchflowerCrop(AbstractBlock.Settings properties) {
